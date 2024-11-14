cec03f0c37a147af43a2b8bc1cd9b11b3804acd5
sandesh300 <bhujbalsandesh52@gmail.com>
2024-11-14 16:06:01 +0530

    add the routing of login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import {APP_INITIALIZER, NgModule} from '@angular/core';
+import { NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-// import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { LoginComponent } from './pages/login/login.component';
+import { LoginComponent } from './pages/login/login.component';
 // import { RegisterComponent } from './pages/register/register.component';
 import {FormsModule} from '@angular/forms';
 import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from '@angular/common/http';
@@ -19,24 +19,24 @@ import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from '@angular/common/h
 @NgModule({
   declarations: [
     AppComponent,
-    // LoginComponent,
+    LoginComponent,
     // RegisterComponent,
     // ActivateAccountComponent
   ],
     imports: [
         BrowserModule,
-        // AppRoutingModule,
+        AppRoutingModule,
         FormsModule,
-        // HttpClientModule,
+        HttpClientModule,
         // CodeInputModule
     ],
   providers: [
-    HttpClient,
+    // HttpClient,
     // {
     //   provide: HTTP_INTERCEPTORS,
     //   useClass: HttpTokenInterceptor,
     //   multi: true
-    // }
+    // },
     // {
     //   provide: APP_INITIALIZER,
     //   deps: [KeycloakService],
