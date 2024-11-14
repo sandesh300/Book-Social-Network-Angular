c6c55ecb4a199c9c4bbbe5b0e315b49e68db9b6d
sandesh300 <bhujbalsandesh52@gmail.com>
2024-11-14 17:44:57 +0530

    add the routing for activate account component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,8 @@ import { RegisterComponent } from './pages/register/register.component';
 import {FormsModule} from '@angular/forms';
 import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from '@angular/common/http';
 // import {HttpTokenInterceptor} from './services/interceptor/http-token.interceptor';
-// import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';
-// import {CodeInputModule} from 'angular-code-input';
+import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';
+import {CodeInputModule} from 'angular-code-input';
 // import {KeycloakService} from './services/keycloak/keycloak.service';
 
 // export function kcFactory(kcService: KeycloakService) {
@@ -21,14 +21,14 @@ import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from '@angular/common/h
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    // ActivateAccountComponent
+    ActivateAccountComponent
   ],
     imports: [
         BrowserModule,
         AppRoutingModule,
         FormsModule,
         HttpClientModule,
-        // CodeInputModule
+         CodeInputModule
     ],
   providers: [
     // HttpClient,
