5c03a4c1b3a9a1efde4a3af2fbce8045b7c79b2e
sandesh300 <bhujbalsandesh52@gmail.com>
2024-11-14 16:51:01 +0530

    add routing to the register component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from './pages/login/login.component';
-// import {RegisterComponent} from './pages/register/register.component';
+import { RegisterComponent } from './pages/register/register.component';
 // import {authGuard} from './services/guard/auth.guard';
 // import {ActivateAccountComponent} from './pages/activate-account/activate-account.component';
 
@@ -15,10 +15,10 @@ const routes: Routes = [
     path: 'login',
     component: LoginComponent
   },
-  // {
-  //   path: 'register',
-  //   // component: RegisterComponent
-  // },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
   // {
   //   path: 'activate-account',
   //   // component: ActivateAccountComponent
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
-// import { RegisterComponent } from './pages/register/register.component';
+import { RegisterComponent } from './pages/register/register.component';
 import {FormsModule} from '@angular/forms';
 import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from '@angular/common/http';
 // import {HttpTokenInterceptor} from './services/interceptor/http-token.interceptor';
@@ -20,7 +20,7 @@ import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from '@angular/common/h
   declarations: [
     AppComponent,
     LoginComponent,
-    // RegisterComponent,
+    RegisterComponent,
     // ActivateAccountComponent
   ],
     imports: [
