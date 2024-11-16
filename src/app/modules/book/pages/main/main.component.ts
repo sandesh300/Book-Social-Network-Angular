beb058f2d172e0a1b93dbad017993a4e33ddd548
sandesh300 <bhujbalsandesh52@gmail.com>
2024-11-16 16:27:55 +0530

    update the code in main component

--- src/app/modules/book/pages/main/main.component.ts
+++ src/app/modules/book/pages/main/main.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main',
-  standalone: true,
-  imports: [],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
