0f6d4a5f05d250dd47b0dae52ad0c7f4d2d79f73
sandesh300 <bhujbalsandesh52@gmail.com>
2024-11-18 15:41:46 +0530

    update the code

--- src/app/modules/book/pages/book-list/book-list.component.ts
+++ src/app/modules/book/pages/book-list/book-list.component.ts
@@ -13,9 +13,9 @@ export class BookListComponent implements OnInit {
   bookResponse: PageResponseBookResponse = {};
   page = 0;
   size = 5;
-  pages: any = [];
-  message = '';
-  level: 'success' |'error' = 'success';
+  // pages: any = [];
+  // message = '';
+  // level: 'success' |'error' = 'success';
 
   constructor(
     private bookService: BookService,
@@ -35,61 +35,61 @@ export class BookListComponent implements OnInit {
       .subscribe({
         next: (books) => {
           this.bookResponse = books;
-          this.pages = Array(this.bookResponse.totalPages)
-            .fill(0)
-            .map((x, i) => i);
+          // this.pages = Array(this.bookResponse.totalPages)
+          //   .fill(0)
+          //   .map((x, i) => i);
         }
       });
   }
 
-  gotToPage(page: number) {
-    this.page = page;
-    this.findAllBooks();
-  }
+  // gotToPage(page: number) {
+  //   this.page = page;
+  //   this.findAllBooks();
+  // }
 
-  goToFirstPage() {
-    this.page = 0;
-    this.findAllBooks();
-  }
+  // goToFirstPage() {
+  //   this.page = 0;
+  //   this.findAllBooks();
+  // }
 
-  goToPreviousPage() {
-    this.page --;
-    this.findAllBooks();
-  }
+  // goToPreviousPage() {
+  //   this.page --;
+  //   this.findAllBooks();
+  // }
 
-  goToLastPage() {
-    this.page = this.bookResponse.totalPages as number - 1;
-    this.findAllBooks();
-  }
+  // goToLastPage() {
+  //   this.page = this.bookResponse.totalPages as number - 1;
+  //   this.findAllBooks();
+  // }
 
-  goToNextPage() {
-    this.page++;
-    this.findAllBooks();
-  }
+  // goToNextPage() {
+  //   this.page++;
+  //   this.findAllBooks();
+  // }
 
-  get isLastPage() {
-    return this.page === this.bookResponse.totalPages as number - 1;
-  }
+  // get isLastPage() {
+  //   return this.page === this.bookResponse.totalPages as number - 1;
+  // }
 
-  borrowBook(book: BookResponse) {
-    this.message = '';
-    this.level = 'success';
-    this.bookService.borrowBook({
-      'book-id': book.id as number
-    }).subscribe({
-      next: () => {
-        this.level = 'success';
-        this.message = 'Book successfully added to your list';
-      },
-      error: (err) => {
-        console.log(err);
-        this.level = 'error';
-        this.message = err.error.error;
-      }
-    });
-  }
+  // borrowBook(book: BookResponse) {
+  //   this.message = '';
+  //   this.level = 'success';
+  //   this.bookService.borrowBook({
+  //     'book-id': book.id as number
+  //   }).subscribe({
+  //     next: () => {
+  //       this.level = 'success';
+  //       this.message = 'Book successfully added to your list';
+  //     },
+  //     error: (err) => {
+  //       console.log(err);
+  //       this.level = 'error';
+  //       this.message = err.error.error;
+  //     }
+  //   });
+  // }
 
-  displayBookDetails(book: BookResponse) {
-    this.router.navigate(['books', 'details', book.id]);
-  }
+  // displayBookDetails(book: BookResponse) {
+  //   this.router.navigate(['books', 'details', book.id]);
+  // }
 }
